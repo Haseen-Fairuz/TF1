16297eaa753cfd0784a0c6dfc00e7c882427c0dc
Haseen Fairuz A R <60533772+Haseen-Fairuz@users.noreply.github.com>
2024-01-19 11:56:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,34 @@
-resource "azurerm_storage_account" "new_sacc" {
-  name                     = "${var.s_acc}demo"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.56.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+variable "rg_name" {
+  type = map(string)
+
+  default = {
+    "dev"   = "dev_rg"
+    "stage" = "stage_rg"
+    "prod"  = "prod_rg"
+  }
+}
+module "ResourceGroup" {
+  source   = "./modules/ResourceGroup"
+  rg_name  = lookup(var.rg_name, terraform.workspace, "dev_rg")
+  location = "eastus"
+}
+module "StorageAccount" {
+  source              = "./modules/StorageAccount"
+  s_acc               = "straccv1"
+  resource_group_name = module.ResourceGroup.rg_name_output
+  location            = "eastus"
 }
